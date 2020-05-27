fc608ad91ed3d9d94ad68a9a65c7275fe38825e8
don-k-jacob <donkjacob.business@gmail.com>
2020-05-27 16:57:15 +0530

    updates

---
+++ lib/const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+TextStyle textStyle=TextStyle(
+    color: Color(0xffF7F7F7),
+    fontWeight: FontWeight.bold,
+    fontSize: 14
+);
--- lib/screens/SignUp.dart
+++ lib/screens/SignUp.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:ecommerceappui/const.dart';
 class SignUP extends StatefulWidget {
   @override
   _SignUPState createState() => _SignUPState();
@@ -35,10 +35,107 @@ class _SignUPState extends State<SignUP> {
              width: 343,
              height: 64,
              decoration: BoxDecoration(
-               color: Colors.grey,
+               color: Color(0xff2A2C36),
                borderRadius: BorderRadius.circular(4)
              ),
-           )
+             child: Center(
+               child: TextField(
+                 decoration: InputDecoration(
+                   border: OutlineInputBorder(),
+                   labelText: 'Name',
+                 ),
+               )
+             ),
+           ),
+            SizedBox(
+              height: 8,
+            ),
+            Container(
+              width: 343,
+              height: 64,
+              decoration: BoxDecoration(
+                  color: Color(0xff2A2C36),
+                  borderRadius: BorderRadius.circular(4)
+              ),
+              child: Center(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                    ),
+                  )
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Container(
+              width: 343,
+              height: 64,
+              decoration: BoxDecoration(
+                  color: Color(0xff2A2C36),
+                  borderRadius: BorderRadius.circular(4)
+              ),
+              child: Center(
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  )
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            GestureDetector(
+              onTap: (){
+
+              },
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text("Already have an account?",style: TextStyle(
+                      color: Color(0xffF7F7F7),
+                        fontSize: 14),),
+                    Icon(Icons.arrow_forward,color: Color(0xffEF3651),),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 28,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 50,
+              decoration: BoxDecoration(
+                  color: Color(0xffEF3651),
+                  borderRadius: BorderRadius.circular(25)
+              ),
+              child: Center(
+                child: Text("Sign Up",
+                style: textStyle),
+              ),
+            ),
+            SizedBox(
+              height: 126,
+            ),
+            Text("Or sign up with social account",
+            style: textStyle.copyWith(fontWeight: FontWeight.normal),),
+            SizedBox(
+              height: 12,
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  
+                )
+              ],
+            )
+
           ],
         ),
       )),
