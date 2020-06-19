22379ba1bb80b3d5097353d5ff9d79a64039779e
don-k-jacob <donkjacob.business@gmail.com>
2020-06-20 04:46:05 +0530

    updates

--- lib/screens/Home/Profile/Settings/Settings.dart
+++ lib/screens/Home/Profile/Settings/Settings.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerceappui/Services/Auth.dart';
 import 'package:ecommerceappui/const.dart';
+import 'package:ecommerceappui/screens/Fpass.dart';
 import 'package:ecommerceappui/screens/Home/Profile/MyOrders/OrderDetails.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +17,146 @@ class _SettingsState extends State<Settings> {
   bool arrivalsIsSwitched = false;
   bool statusIsSwitched = false;
 
+  String pass='';
+  void _settingModalBottomSheet(context){
+    showModalBottomSheet(
+        backgroundColor: Colors.transparent,
+        context: context,
+        builder: (BuildContext bc){
+          return Container(
+            height: 472,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Color(0xff1E1F28),
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(34),
+                    topRight: Radius.circular(34))
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 14,
+                  ),
+                  Container(
+                    height: 6,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      color: Color(0xffABB4BD),
+                      borderRadius: BorderRadius.all(Radius.circular(3)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Text("Password Change",style: h2Style,),
+                  SizedBox(
+                    height: 18,
+                  ),
+                  Container(
+                    color: Color(0xff2A2C36),
+                    child: TextFormField(
+                      validator: (val)=> val.length<6 ?"enter a password"
+                          "6+ char long":null,
+                      onChanged: (val){
+                        setState(() {
+                          pass=val;
+                        });
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 14,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Fpass()),
+                      );
+                    },
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            "Forgot your password?",
+                            style: TextStyle(color: Color(0xffF7F7F7), fontSize: 14),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 18,
+                  ),
+                  Container(
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          color: Color(0xff2A2C36),
+                          child: TextFormField(
+                            validator: (val)=> val.length<6 ?"enter a password"
+                                "6+ char long":null,
+                            onChanged: (val){
+                              setState(() {
+                                pass=val;
+                              });
+                            },
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Password',
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 24,
+                        ),
+                        Container(
+                          color: Color(0xff2A2C36),
+                          child: TextFormField(
+                            validator: (val)=> val.length<6 ?"enter a password"
+                                "6+ char long":null,
+                            onChanged: (val){
+                              setState(() {
+                                pass=val;
+                              });
+                            },
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Password',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  Container(
+                    height: 48,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: Color(0xffEF3651),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Center(child: Text("SAVE PASSWORD",style: h4Style.copyWith(fontWeight: FontWeight.normal),)),
+                  )
+                ],
+              ),
+            ),
+          );
+        }
+    );
+  }
   @override
   void initState() {
     // TODO: implement initState
@@ -226,42 +367,3 @@ class _SettingsState extends State<Settings> {
 
 
 
-void _settingModalBottomSheet(context){
-  showModalBottomSheet(
-      backgroundColor: Colors.transparent,
-      context: context,
-      builder: (BuildContext bc){
-        return Container(
-          height: 472,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              color: Color(0xff1E1F28),
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(34),
-                  topRight: Radius.circular(34))
-          ),
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 14,
-              ),
-              Container(
-                height: 6,
-                width: 60,
-                decoration: BoxDecoration(
-                  color: Color(0xffABB4BD),
-                  borderRadius: BorderRadius.all(Radius.circular(3)),
-                ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Text("Password Change",style: h2Style,),
-              SizedBox(
-                height: 18,
-              ),
-            ],
-          ),
-        );
-      }
-  );
-}
